220e1f74ab516750792b91e020543a3d4f813fb3
vijaypanchalr3 <vijaypanchalr3@users.noreply.github.com>
2022-01-28 13:24:29 +0000

    deploy: ae1e9f8760ce213657202eb392a3c5e42515f491

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
 
 let scrollper = () => {
   let docEl = document.documentElement,
-      docbody = document.docbody,
-      scrolltop = docEl['scrollTop'] || docbody['scrollTop'],
-      scrollbot = (docEl['scrollHeight'] || docbody['scrollHeight'])- window.innerHeight,
-      scrollpercent = scrolltop / scrollbot * 100 + '%';
-
-      document.getElementById('progressbar').style.setProperty('--scrollamount',scrollpercent);
+  docbody = document.docbody,
+  scrolltop = docEl['scrollTop'] || docbody['scrollTop'],
+  scrollbot = (docEl['scrollHeight'] || docbody['scrollHeight'])- window.innerHeight,
+  scrollpercent = scrolltop / scrollbot * 100 + '%';
+  // divElement = document.getElementById('page_toc');
+  // elemHeight = divElement.offsetHeight;
+  // elemupHeight = divElement.offsetTop;
+  // if (scrolltop >= (elemupHeight+elemHeight)) {
+  //   document.getElementById('navbar').classList.add("none");
+  // } else {
+  //   document.getElementById('navbar').classList.remove("none");
+  // }
+  document.getElementById('progressbar').style.setProperty('--scrollamount',scrollpercent);
 }
 
 document.addEventListener('scroll',scrollper);
-
-
