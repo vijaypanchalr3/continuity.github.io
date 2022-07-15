0a4f37c4b7730c4b4e3b590caea3f867aeddc7b9
vijay panchal <vijay@debian.vijay>
2022-07-15 11:26:29 +0530

    footer

--- static/js/main.js
+++ static/js/main.js
@@ -52,3 +52,11 @@ navlinks.forEach(link => {
   })
 })
 
+function copy(that){
+  var inp =document.createElement('input');
+  document.body.appendChild(inp)
+  inp.value =that.textContent
+  inp.select();
+  document.execCommand('copy',false);
+  inp.remove();
+  }
