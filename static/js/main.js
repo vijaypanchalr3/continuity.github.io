1244546a4c50e7fb45a1a8e1b3104c2435c137f6
vijay panchal <vijay@debian.vijay>
2022-07-15 20:19:10 +0530

    related page

--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,11 @@
 
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-
 let scrollper = () => {
   let docEl = document.documentElement,
-  docbody = document.docbody,
-  scrolltop = docEl['scrollTop'] || docbody['scrollTop'],
-  scrollbot = (docEl['scrollHeight'] || docbody['scrollHeight'])- window.innerHeight,
+  docbod = document.docbody,
+  scrolltop = docEl['scrollTop'] || docbod['scrollTop'],
+  scrollbot = (docEl['scrollHeight'] || docbod['scrollHeight'])- window.innerHeight,
   scrollamount = scrolltop / scrollbot
   scrollpercent = scrollamount * 100 + '%';
-  // divElement = document.getElementById('page_toc');
-  // elemHeight = divElement.offsetHeight;
-  // elemupHeight = divElement.offsetTop;
-  // if (scrolltop >= (elemupHeight+elemHeight)) {
-  //   document.getElementById('navbar').classList.add("none");
-  // } else {
-  //   document.getElementById('navbar').classList.remove("none");
-  // }
   if (scrollamount > 0.1) {
     document.querySelector('.goto-top').classList.add('none')
   } else {document.querySelector('.goto-top').classList.remove('none')}
